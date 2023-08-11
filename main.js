cfb0e97dbe9688b3c79d0961ef3098a88fb65181
SomSingh23 <githubsom@gmail.com>
2023-08-11 19:55:13 +0530

    ui changed , copy button added

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+function copyToClipboard(text) {
+  const tempInput = document.createElement("input");
+  tempInput.value = text;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
+}
+const copyButtons = document.querySelectorAll(".copy-btn");
+copyButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const linkId = button.getAttribute("data-link");
+    const linkText = document.getElementById(linkId).textContent;
+    copyToClipboard(linkText);
+    button.textContent = "Copied!";
+    setTimeout(() => {
+      button.textContent = "Copy";
+    }, 1500);
+  });
+});
+
 let call_api = async (token) => {
   try {
     let response = await axios.get(
@@ -10,13 +31,13 @@ let call_api = async (token) => {
     let _link1 = document.querySelector("#link1");
     let _link2 = document.querySelector("#link2");
     let _link3 = document.querySelector("#link3");
-    main3.style.border = "1px solid #4f4831";
+    // main3.style.border = "1px solid #4f4831";
     _link1.textContent = "";
     _link2.textContent = "";
     _link3.textContent = "";
-    _link1.textContent = `Link 1:  ${link1}`;
-    _link2.textContent = `Link 2:  ${link2}`;
-    _link3.textContent = `Link 3:  ${link3}`;
+    _link1.textContent = `${link1}`;
+    _link2.textContent = `${link2}`;
+    _link3.textContent = `${link3}`;
   } catch (err) {
     let _link1 = document.querySelector("#link1");
     _link1.textContent = "";
