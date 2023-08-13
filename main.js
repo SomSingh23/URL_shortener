876d2d33c6bc8e47e938f38e7f7f2b018bf5434d
SomSingh23 <githubsom@gmail.com>
2023-08-13 12:15:48 +0530

    The third-party API has been removed, our own API has been added, and there have been some minor changes made to both the 'main.js' and 'index.html' files.

--- main.js
+++ main.js
@@ -22,22 +22,15 @@ copyButtons.forEach((button) => {
 let call_api = async (token) => {
   try {
     let response = await axios.get(
-      `https://api.shrtco.de/v2/shorten?url=${token}`
+      `https://som-745j.onrender.com?url=${token}`
     );
-    const link1 = response.data.result.short_link;
-    const link2 = response.data.result.short_link2;
-    const link3 = response.data.result.short_link3;
+    const link1 = response.data.shortUrl;
+
     let main3 = document.querySelector(".main3");
     let _link1 = document.querySelector("#link1");
-    let _link2 = document.querySelector("#link2");
-    let _link3 = document.querySelector("#link3");
     // main3.style.border = "1px solid #4f4831";
     _link1.textContent = "";
-    _link2.textContent = "";
-    _link3.textContent = "";
     _link1.textContent = `${link1}`;
-    _link2.textContent = `${link2}`;
-    _link3.textContent = `${link3}`;
   } catch (err) {
     let _link1 = document.querySelector("#link1");
     _link1.textContent = "";
